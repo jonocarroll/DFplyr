7908ad1ed01892b425a2cca9574ddddc2182e22c
Jonathan Carroll <jono@jcarroll.com.au>
2019-12-06 13:29:00 +1030

    preparing for altering printing

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onLoad <- function(libname, pkgname) {
+  # op <- options()
+  # op.DFplyr <- list(
+  #   DFplyr.print_as = "DataFrame"
+  # )
+  # toset <- !(names(op.DFplyr) %in% names(op))
+  # if (any(toset)) options(op.DFplyr[toset])
+
+  invisible()
+}
