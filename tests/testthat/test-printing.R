2d56e1ba98f54d85af455c1a5ac5a5ee346129d1
Jonathan Carroll <github@jcarroll.com.au>
2024-09-11 23:33:58 +0930

    test ungrouped show method

--- tests/testthat/test-printing.R
+++ tests/testthat/test-printing.R
@@ -1,7 +1,15 @@
-test_that("show method works", {
+test_that("ungrouped show method works", {
+    d <- S4Vectors::DataFrame(mtcars)
+    out <- capture.output(d)
+    expect_identical(out[1], "DataFrame with 32 rows and 11 columns")
+    expect_identical(substr(out[2], 1, 27),  "                        mpg")
+})
+
+test_that("grouped show method works", {
     d <- S4Vectors::DataFrame(mtcars)
     dg <- group_by(d, am)
     out <- capture.output(dg)
     expect_identical(out[1], "DataFrame with 32 rows and 11 columns")
     expect_identical(out[2], "Groups:  am ")
+    expect_identical(substr(out[3], 1, 27),  "                        mpg")
 })
